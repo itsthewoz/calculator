ffa4033e75eef0e84cde5af7555fdb5282ea9a5f
Przemek Wozniak <pkwznk@gmail.com>
2022-02-02 14:10:41 +0100

    base html and css added

---
+++ javascript/scripts.js
@@ -0,0 +1,9 @@
+//vars for display value
+//vars to hold numbers
+
+//add function
+//subtract function
+//multiply function
+//devide function
+//operate function that takes an operator and 2 numbers and calls an above function
+//clear function
